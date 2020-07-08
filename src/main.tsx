80cd8815bd0d1de4ced0096164c4d6092375b5e2
Anthony Gubler <agubler@sitepen.com>
2020-07-08 16:35:39 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+import { renderer, create, tsx } from "@dojo/framework/core/vdom";
+
+const factory = create();
+
+const App = factory(function App() {
+  return (
+    <div>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  );
+});
+
+const r = renderer(() => <App />);
+r.mount();
